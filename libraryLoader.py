49594a4c261037c7b86bd84963dd889b456c8671
Ping-Keng Jao <ping-keng.jao@alumni.epfl.ch>
2021-05-06 17:06:59 +0800

    update explanation, deal with an issue with the newline character, and made the code thinner.

--- libraryLoader.py
+++ libraryLoader.py
@@ -5,15 +5,26 @@ Created on Sun May  2 22:05:38 2021
 @author: Ping-Keng Jao
 
 Purpose of file:
-    for adding paths from a config file, suitable for lazy persons :)
+    For adding paths from a config file, suitable for lazy persons :)
     
-    Example of scenario 1:
-        Running the same project on different machines with different library path.
-        The script can run the formattedLoader to load a corresponding config file based on the information of the machine.
+    When to use:
+        You have some self developed codes/package that will be used in different projects or machines.
+        Your codes are not in the search path nor installed. Then, you will need to add the path manually before running your script.
+        However, you will need to have this .py in a searchable folder or the same folder as your script to import and run it.
+    
+        Scenario 1:
+            Running the same project on different machines with different library path.
+            The script can run the formattedLoader to load a corresponding config file based on the information of the machine.
+            
+        Scenario 2:
+            You don't want to write so many path information in a .py file when importing.
+            Run add_path_from_file() and remember to set verbose=False if wish not to see the info on the terminal.
         
-    Example of scenario 2:
-        You don't want to see so many path information in a .py file when importing.
-        Run add_path_from_file() and remember to set verbose=False if wish not to see the info on the terminal.
+    How should a config file looks like:
+        Each line is a path to your library folder. The path can be absolute or relative, depends on your need.
+        E.g:
+            C:\Python\MyLibraries\tool1
+        
 """
 
 import os
@@ -45,12 +56,13 @@ def formattedLoader(prefix='library_path', proj_name='', file_format=['proj', 'O
 
 def add_path_from_file(file: str, priority=1, verbose=True):
     with open(file) as f:
-        lines = f.readlines()
+        lines = f.read().splitlines()
     print(f'\x1b[42mLoading: {file}\x1b[0m')
     for l in lines:        
         if l not in sys.path: # no need to add multiple times
-            if not os.isdir(l):
-                print(f'\x1b[41mConfig file contains a non-folder line: {l}\x1b[0m')
+            if not os.path.isdir(l):
+                print(f'\x1b[41mConfig file contains a non-folder line and will be skipped: {l}\x1b[0m')                
+                continue
             sys.path.insert(priority, l)
             if verbose:
                 print(f"adding path: {l}", end='')
@@ -63,18 +75,13 @@ def remap_keyword(k: str) -> str:
                'User': ['user', 'uid', 'id'],
                'Project': ['proj', 'project', 'pro', 'project name', 'pname', 'p name']
               }
-    if kl in synonym['OS']:
-        return 'OS'
-    elif kl in synonym['Computer Name']:
-        return 'Computer Name'
-    elif kl in synonym['User']:
-        return 'User'
-    elif kl in synonym['Project']:
-        return 'Project'
-    else:
-        print(f'\x1b[41mUnknown keyword: {k}\x1b[0m')
-        print('\x1b[41mPlease find the supported case-insensitive keyword from the list below:\x1b[0m')
-        print(f'\x1b[41m{synonym}\x1b[0m')
+    for s in synonym.keys():
+        if kl in synonym[s]:
+            return s
+    # if not found in synonyms, it is an error
+    print(f'\x1b[41mUnknown keyword: {k}\x1b[0m')
+    print('\x1b[41mPlease find the supported case-insensitive keyword from the list below:\x1b[0m')
+    print(f'\x1b[41m{synonym}\x1b[0m')
 
 def get_OS() -> str:
     s = platform.system() # Linux, Darwin, Windows
