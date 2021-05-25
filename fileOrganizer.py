28f966a67aaf01efe87dc3eca9010e53a1403c9b
Ping-Keng Jao <ping-keng.jao@alumni.epfl.ch>
2021-05-26 00:58:20 +0800

    add some functions to save and load files. Config file now supports comments.

--- fileOrganizer.py
+++ fileOrganizer.py
@@ -17,6 +17,50 @@ import os
 import re
 from natsort import natsorted
 from libraryLoader import formattedFileName
+try:
+    import cPickle as pickle
+except ModuleNotFoundError:
+    import pickle
+
+def filesep(fullname):
+    dirname = os.path.dirname(fullname)
+    filename = os.path.basename(fullname)
+    filename, ext = os.path.splitext(filename)
+    return dirname, filename, ext
+
+def remove_target_str(name: str, target_str) -> str:
+    if isinstance(target_str, str):
+        target_str = [target_str]
+    for target in target_str:
+        idx = name.find(target)
+        l = len(target)
+        if idx > -1:
+            name = name[: idx] + name[idx+l: ]
+    return name
+
+def make_folder_if_necessary(filename):
+    dirname = os.path.dirname(filename)
+    if not os.path.exists(dirname):
+        try:
+            os.makedirs(dirname)
+        except FileExistsError:            
+            pass # directory already exists by maybe other threads or programs
+
+def save_data_as_file(filename, **kwargs):
+    saved_vars = {}
+    if filename[-4:] != '.pkl':
+        print(f'adding extension (.pkl) to {filename}')
+        filename += '.pkl'        
+    for key, value in kwargs.items():
+        saved_vars[key] = value
+    make_folder_if_necessary(filename)
+    with open(filename, 'wb') as f:  # Overwrites any existing file.
+        pickle.dump(saved_vars, f, pickle.HIGHEST_PROTOCOL)
+
+def load_data_file(filename):
+    with open(filename, 'rb') as f:
+        data = pickle.load(f)
+    return data
 
 def search_files_from_root_folder(root, expression, verbose=False):
     # using regular expression to search all files at and below the root folder
@@ -64,23 +108,51 @@ class FileOrganizer:
         self.config = config
         with open(config) as f:
             lines = f.read().splitlines()
+        keywords = ['name_purpose', 'root_path', 'prefix', 'postfix', 'ext']
+        d = {}
+        count = 0
+        for l in lines:         
+            if len(l) > 0 and l[0] == '#': # begin with a comments, skip this line
+                continue
+            idx_comment = str.find(l, '#') # remove strings from the # symbol
+            if idx_comment != -1:
+                l = l[0: idx_comment]
+            l = l.rstrip() # remove white space at the end
+            d[keywords[count]] = l
+            count += 1
+            if count == 5:
+                self.add_path(**d)
+                count = 0
+        ''' old way to read a config file without comments
         for i in range(0, len(lines), 5):
             name_purpose = lines[i]
             root_path = lines[i+1]
             prefix = lines[i+2]
             postfix = lines[i+3]
             ext = lines[i+4]
-            self.add_path(name_purpose, root_path, prefix, postfix, ext)            
+            self.add_path(name_purpose, root_path, prefix, postfix, ext)        
+        '''
     
-    def add_path(self, name_purpose, root_path, prefix='', postfix='', ext=''):
+    def add_path(self, name_purpose, root_path, prefix='', postfix='', ext=''):        
+        self.paths[name_purpose] = (root_path, prefix, postfix, ext)
+    
+    def get_full_path(self, name_purpose, folder_name, filename):
+        name_set = self.paths[name_purpose]
+        full_filename = filename
+        if name_set[1] != '':
+            full_filename = name_set[1] + '_' + full_filename
+        if name_set[2] != '':
+            full_filename += '_' + name_set[2]
+        full_filename += '.' + name_set[3]
+        full_path = os.path.join(name_set[0], folder_name, full_filename)
+        return full_path
+    
+    def get_files(self, name, only_at_root=False, verbose=False):
+        root_path, prefix, postfix, ext = self.paths[name]
         if ext[0] == '.':
             ext = '\\' + ext
         elif ext[0] != '\\':
             ext = '\.' + ext
-        self.paths[name_purpose] = (root_path, prefix, postfix, ext)
-    
-    def get_files(self, name, only_at_root=False, verbose=False):
-        root_path, prefix, postfix, ext = self.paths[name]
         expression = prefix + '.*' + postfix + ext
         if only_at_root:
             files_matched = search_files_at_root_folder(root_path, expression, verbose)
@@ -97,8 +169,8 @@ class FileOrganizer:
         return self.paths[name]
     
 if __name__ == '__main__':    
-    fo = FileOrganizer('./data_path_EEG_G20-PK.cfg')
-    fo.load_config_with_formatted_name(proj_name='EEG')
+    fo = FileOrganizer('./data_path_BCI2019_G20-PK.cfg')
+    fo.load_config_with_formatted_name(proj_name='BCI2019')
     files = fo.get_files('high lv')
     print(files)
     print(fo.get_name('high lv', 'testname', '_'))
